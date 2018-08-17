275f3b4c72af1887d8cc25b49913ac26b56eb318
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-17 19:56:38 +0300

    modified user.ts class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,4 @@
 export class User {
-  constructor(public name:string, public login: string, public avatar_url:string, public description:string,public public_repos:number , public html_url: string, public followers:number ,public following:number){
+  constructor(public name:string, public login: string, public avatar_url:string){
   }
 }
