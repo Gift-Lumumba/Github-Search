b0f8af8169576066c88f0287aa5650021fbcb245
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-17 20:41:39 +0300

    modified and updated profile service

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,43 @@
+
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import {environment} from '../../environments/environment'
+import { User } from '../user';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn:'root'
 })
 export class ProfileService {
+user:User;
 
-  constructor() { }
+constructor(private http:HttpClient) { 
+this.user=new User('','','');
+  }
+
+getProfileInfo(username){
+  interface ApiResponse{
+    user:string;
+    login:string;
+    avatar_url:string;
+    
+}
+const promise =new Promise((resolve,reject)=>{
+  this.http.get<ApiResponse>(environment.apiUrl + username + environment.apikey).toPromise().then(response => {
+      
+      this.user.name=response.user;
+      this.user.login=response.login;
+      this.user.avatar_url=response.avatar_url;
+
+      resolve()
+    },
+
+           
+        
+        )
+    })
+
+    return promise
+  
+}
 }
