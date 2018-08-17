7af569bed16a8918657a427447962e3ce2010c73
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-17 19:55:40 +0300

    modified and updatedProfileComponent

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
-import { Repos} from '../repos';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-profile',
@@ -8,10 +8,21 @@ import { Repos} from '../repos';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user:User;
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+  }
 
   ngOnInit() {
+
+    interface ApiResponse{
+        user:string;
+        login:string
+        avatar_url:string;
+    }
+    this.http.get<ApiResponse>("https://api.github.com/users/Gift-Lumumba?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e").subscribe(data=>{
+        this.user= new User(data.user,data.login,data.avatar_url)
+    })
   }
 
 }
