1ce75a3eb8e9222d25f247cbe54aecae5b71b7f3
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-18 01:42:18 +0300

    modified and updated component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { ProfileService } from '../services/profile.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+
+  providers: [ProfileService]
 })
 export class ProfileComponent implements OnInit {
+  public username ='Gift-Lumumba';
   user:User;
 
   constructor(private profileService:ProfileService) { }
 
   ngOnInit() {
+    this.profileService.getProfileInfo(this.username);
+    this.user = this.profileService.user;
   }
 
 }
