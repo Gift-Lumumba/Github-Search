be9915af8d6805cef7563125d474caf11260e8a7
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-18 04:47:04 +0300

    modified component

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../services/profile.service';
 import { Search } from '../search';
 
+
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
-  newSearch = new Search('');
+  newSearch = new Search('','','',0);
   
   submitUser(name){
     this.profileService.getProfileInfo(name.value)
+
   }
 
   constructor(private profileService:ProfileService) { }
