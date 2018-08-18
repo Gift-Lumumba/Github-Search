4d4abeaa13fdbf6d0c301139048a321e606e8d70
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-18 04:51:00 +0300

    stored apiUrl and apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl:'https://api.github.com/users/',
-  apikey:'?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e'
+  apikey:'?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e',
 };
 
 /*
