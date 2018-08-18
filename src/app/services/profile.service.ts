5e0d67435238d4e9e66932cc39e3c45ec4141ee2
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-18 04:49:12 +0300

    added promises

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import {environment} from '../../environments/environment'
 import { User } from '../user';
+import { Repos } from '../repos';
 
 
 @Injectable({
@@ -10,9 +11,10 @@ import { User } from '../user';
 })
 export class ProfileService {
 user:User;
+repos:Repos;
 
 constructor(private http:HttpClient) { 
-this.user=new User('','','');
+this.user=new User('','','',0,'','','','','');
   }
 
 getProfileInfo(username){
@@ -20,6 +22,12 @@ getProfileInfo(username){
     name:string;
     login:string;
     avatar_url:string;
+    public_repos:number;
+    followers:string;
+    following:string;
+    email:string;
+    location:string;
+    html_url:string;
     
 }
 const promise =new Promise((resolve,reject)=>{
@@ -28,6 +36,12 @@ const promise =new Promise((resolve,reject)=>{
       this.user.name=response.name;
       this.user.login=response.login;
       this.user.avatar_url=response.avatar_url;
+      this.user.public_repos=response.public_repos;
+      this.user.followers=response.followers;
+      this.user.following=response.following;
+      this.user.email=response.email;
+      this.user.location=response.location;
+      this.user.html_url=response.html_url;
 
       resolve()
     },
