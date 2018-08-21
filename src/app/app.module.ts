276c9bb48ff0c6029399d64aff8400a8719f61e1
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-21 14:42:16 +0300

    imported NgProgressModule and NgProgressHttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -16,9 +18,12 @@ import {FormsModule} from '@angular/forms';
     ReposComponent,
     SearchFormComponent,
   ],
+
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule,
     FormsModule
   ],
   providers: [ProfileService],
